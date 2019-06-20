f6de72408ab6ed994c6bb205f053c89e1607ea06
M D SOW <mdiansow.prestataire@si2m.fr>
2019-06-20 18:56:46 +0200

    Init Spring gateway project

---
+++ spring-gateway/src/main/kotlin/fr/mdsow/springgateway/SpringGatewayApplication.kt
@@ -0,0 +1,11 @@
+package fr.mdsow.springgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringGatewayApplication>(*args)
+}
